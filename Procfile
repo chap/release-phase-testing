8be2b11f656549e552c687c4a5c7c50e4752073e
Chap Ambrose <chapambrose@gmail.com>
2018-01-16 09:41:28 -0600

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+release: ruby loop-printer.rb
---
+++ loop-printer.rb
@@ -0,0 +1,9 @@
+current = 0
+limit = 30
+delay = 10
+
+while current < limit do
+  puts "#{current}/#{limit} - #{Time.now}"
+  current += 1
+  sleep(delay)
+end
